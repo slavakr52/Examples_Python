4e696fd19dc34c9628a58c4f8abedb44b6f04686
slavakr52 <slavakr52@mail.ru>
2023-01-30 16:00:19 +0300

    init commit

---
+++ Example006_Flask/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "<h1>Hello world</h1><br><a href='/index'>перейти на 2-ю страницу</a>"
+
+@app.route('/index/<x>/<y>')
+def index(x, y):
+    return f'Результат: {int(x) + int(y)}'
+
+if __name__ == '__main__':
+    app.run()
