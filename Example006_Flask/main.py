6b506016f9b38e002b8b4e2843f96e440931749d
slavakr52 <slavakr52@mail.ru>
2023-02-01 16:55:21 +0300

    example 006 update

--- Example006_Flask/main.py
+++ Example006_Flask/main.py
@@ -1,20 +1,63 @@
 from flask import Flask, render_template
-from random import randint as rd
+from LoginForm import Lf
+from AuthForm import AuthF
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello world'
 
 @app.route('/')
 def main():
-    with open('file.txt', 'r', encoding = 'utf-8') as file:
-        resultData = list()
-        for line in file.readlines():
-            resultData.append(tuple(line.split('\n')[0].split(';')))
+    return render_template ('base.html')
 
-    return render_template('base.html', data = resultData)
+@app.route('/register', methods=['GET', 'POST'])
+def reg():
+    form = Lf()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                    message='Пароли не совпадают!')
+        with open('file.txt', 'a', encoding='utf-8') as file:
+            file.write(f"{form.name.data};{form.email.data};{form.password.data}\n")
+        return render_template('register.html', message='Регистрация прошла успешно!')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    form = AuthF()
+    if form.validate_on_submit():
+        with open('file.txt', 'r', encoding='utf-8') as file:
+            data = ' '.join(file.readlines())
+
+        if form.email.data not in data:
+            return render_template ('login.html', form=form, message='Вы не зарегистрированы!')
+        else:
+            for i in data.split():
+                if form.email.data in i:
+                    if i.split(';')[-1] == form.password.data:
+                        return render_template ('login.html', form=form, message='Вы авторизовались!')
+
+
+    return render_template ('login.html', form=form)
+
+
+    
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+#     @app.route('/')
+# def main():
+#     with open('file.txt', 'r', encoding = 'utf-8') as file:
+#         resultData = list()
+#         for line in file.readlines():
+#             resultData.append(tuple(line.split('\n')[0].split(';')))
+
+#     return render_template('base.html', data = resultData)
+
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
