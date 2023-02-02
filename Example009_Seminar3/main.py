232fffe9e749c37323f014b314eda59533b9b26d
slavakr52 <slavakr52@mail.ru>
2023-02-02 22:44:28 +0300

    seminar 3 update

---
+++ Example009_Seminar3/main.py
@@ -0,0 +1,31 @@
+# Дана последовательность из N целых чисел и число K. 
+# Необходимо сдвинуть всю последовательность (сдвиг - циклический) 
+# на K элементов вправо, K – положительное число.
+
+# list = [1, 2, 3, 4, 5]
+# number = int(input('Введите число, на которое сдвинем массив: '))
+# i = 0
+# while i < number:
+#     list.insert(0, list[len(list)-1])
+#     list.pop(len(list)-1)
+#     i+=1
+# print(list)
+
+
+
+# Напишите программу для печати всех уникальных значений в словаре.
+
+# dict = {}
+# unique = set()
+# dict['g'] = 'hello'
+# dict['f'] = 'hello'
+# dict['h'] = 'python'
+# dict['d'] = 'world'
+# dict['s'] = 'world'
+# dict['a'] = 'python'
+
+# for i in dict:
+#     for key in i:
+#         unique.add(i[key])
+# print(unique)
+
